d2d296232e79ba155164d1b0352e102d600596fb
Arraval <cody.coombs@live.ca>
2024-11-25 11:14:47 -0330

    Initial project structure and css properties

---
+++ app.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <NuxtLayout class="main">
+    <NuxtPage :page-key="route => route.fullPath" />
+  </NuxtLayout>
+</template>
---
+++ components/mainFooter.vue
@@ -0,0 +1,10 @@
+<script setup>
+	
+</script>
+
+<template>
+	<div class="footer">
+		<div class="header-footer-item">This is a fan-made project and has no affiliation with Brighter Shores or Fen Research.</div>
+		<div class="header-footer-item" style="text-align:right">GitHub Button</div>
+	</div>
+</template>
